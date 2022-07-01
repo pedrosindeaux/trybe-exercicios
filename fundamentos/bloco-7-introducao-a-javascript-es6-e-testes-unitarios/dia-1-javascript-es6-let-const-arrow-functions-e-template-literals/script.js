817a4cbc0fa60338d4cd7ad69a2d0eabc1e4cef5
pedrosindeaux <pedroobraga@gmail.com>
2022-07-01 14:48:30 -0300

    exercicios 7.1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -29,4 +29,61 @@
 
   testingScope(true);
 
+  
   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
+
+
+
+
+/* const sum = (value1, value2) => value1 + value2;
+
+console.log(sum(2,'3')) */
+
+
+
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+/* let newKey = 'lastName';
+const lastName = 'Ferreira';
+customer[newKey] = lastName;
+newKey = 'fullName';
+const fullName = `${customer.firstName} ${customer.lastName}`;
+customer[newKey] = fullName;
+console.log(customer); */
+
+
+const estudante = (nome, cidade,age) => {
+    const tryber ={
+    nome:'',
+    cidade:'',
+    age:'',
+    }  
+} 
+
+
+console.log('Pedro','bh',18);
+
+
+ const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+}; 
+
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+console.log(sum(2, 3));
